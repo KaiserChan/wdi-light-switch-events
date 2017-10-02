c284cae55783715dd66788e9413d6f2cc24de9fa
KaiserChan <chankaiserkk@gmail.com>
2017-10-02 17:29:44 -0400

    updated light switch exercise ... completed main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
 
+  var button = document.querySelector('button');
+  var lighting = document.querySelector('body');
+  var switchStatus = document.querySelector('.switch.on')
+  var text = document.querySelector('.status');
 
+  function clickButton() {
+    if (lighting.className === 'light') {
+      lighting.className = 'dark';
+      switchStatus.className = 'switch off';
+      text.innerHTML = 'Who turned off the lights?';
+    } else {
+      lighting.className = 'light';
+      switchStatus.className = 'switch on';
+      text.innerHTML = "It's so bright in here!";
+    };
+  };
+
+  button.addEventListener('click', clickButton);
+
+});
